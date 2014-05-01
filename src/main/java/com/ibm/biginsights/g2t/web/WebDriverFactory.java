53c2f0ece274c42f195c71cf5455ba71916d0646
Chao Ren <chaoren@us.ibm.com>
2014-04-30 21:20:39 -0700

    Support IE
    Download IEDriver from
    32 bit:
    http://selenium-release.storage.googleapis.com/2.41/IEDriverServer_Win32
    _2.41.0.zip
    64 bit:
    http://selenium-release.storage.googleapis.com/2.41/IEDriverServer_x64_2
    .41.0.zip
    
    Specify system property "webdriver.ie.driver" to the binary file
    that extracted from the zip.

--- src/main/java/com/ibm/biginsights/g2t/web/WebDriverFactory.java
+++ src/main/java/com/ibm/biginsights/g2t/web/WebDriverFactory.java
@@ -2,12 +2,19 @@ package com.ibm.biginsights.g2t.web;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class WebDriverFactory
 {
     public static WebDriver getSeleniumWebDriver(Browsers browser)
     {
-        WebDriver driver = new FirefoxDriver();
-        return driver;
+        switch (browser) {
+          case FIREFOX:
+            return new FirefoxDriver();
+          case IE:
+            return new InternetExplorerDriver();
+          default:
+            return new FirefoxDriver();
+        }
     }
 }
